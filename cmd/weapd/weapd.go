f16864407f5fb1615aeb7fa36969aa9968c09054
Jonathan Rudenberg <jonathan@titanous.com>
2019-08-06 18:13:32 -0400

    explicitly set TLS config

--- cmd/weapd/weapd.go
+++ cmd/weapd/weapd.go
@@ -18,8 +18,12 @@ func main() {
 	keyFile := flag.String("key", "server-key.pem", "server TLS certificate key file")
 	caFile := flag.String("ca", "ca.pem", "CA certificates file to trust for client auth")
 	authKey := flag.String("authkey", "", "Message-Authenticator shared secret key")
+	insecureTLS := flag.Bool("insecure-tls", false, "enable weak and broken TLS versions and ciphers")
 	listenAddr := flag.String("listen", ":1812", "UDP listen address")
 	flag.Parse()
+
+	// enable TLS 1.3
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 
 	l := log.New(os.Stdout, "", log.Lmicroseconds|log.Lshortfile)
 
@@ -39,7 +43,37 @@ func main() {
 		Certificates:           []tls.Certificate{cert},
 		ClientCAs:              caPool,
 		ClientAuth:             tls.RequireAndVerifyClientCert,
-		SessionTicketsDisabled: true,
+		SessionTicketsDisabled: true,                                     // resumed connections do not cache client certificate details
+		CurvePreferences:       []tls.CurveID{tls.X25519, tls.CurveP256}, // enable only curves with constant-time ASM implementations on amd64
+		MinVersion:             tls.VersionTLS12,                         // versions before TLS 1.2 have known weaknesses
+		CipherSuites: []uint16{ // only allow forward-secret ciphers with no known weaknesses
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
+	}
+	if *insecureTLS {
+		config.MinVersion = tls.VersionTLS10
+		config.CipherSuites = append(config.CipherSuites,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_RSA_WITH_RC4_128_SHA,
+		)
 	}
 	tlsServer, err := eaptls.NewServer(&eaptls.ServerConfig{
 		TLSConfig: config,
